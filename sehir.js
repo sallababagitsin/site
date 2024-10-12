37edcf2012e888e2cf25705feffcf50a4e44dea4
sallababagitsin <neyapimcom@gmail.com>
2024-10-12 16:25:53 +0300

    Create sehir.js

---
+++ sehir.js
@@ -0,0 +1,112 @@
+const sehirler = [
+    {
+        name: "İstanbul",
+        population: 15519267,
+        description: "İstanbul, Türkiye'nin en kalabalık şehri ve tarih boyunca birçok medeniyete ev sahipliği yapmıştır.",
+        landmarks: ["Ayasofya", "Topkapı Sarayı", "Galata Kulesi"]
+    },
+    {
+        name: "Ankara",
+        population: 5503985,
+        description: "Ankara, Türkiye'nin başkenti olup, ülkenin ikinci en büyük şehridir.",
+        landmarks: ["Anıtkabir", "Kocatepe Camii", "Atatürk Orman Çiftliği"]
+    },
+    {
+        name: "İzmir",
+        population: 4320519,
+        description: "İzmir, Ege Bölgesi'nde yer alan, Türkiye'nin üçüncü büyük şehri ve önemli bir liman kentidir.",
+        landmarks: ["İzmir Saat Kulesi", "Kordon", "Efes Antik Kenti"]
+    },
+    {
+        name: "Bursa",
+        population: 3057120,
+        description: "Bursa, Osmanlı İmparatorluğu'nun ilk başkenti olan tarihi bir şehirdir.",
+        landmarks: ["Uludağ", "Bursa Kalesi", "Yeşil Camii"]
+    },
+    {
+        name: "Antalya",
+        population: 2542000,
+        description: "Antalya, Türkiye'nin güney sahilinde yer alan ve turistik bir tatil beldesidir.",
+        landmarks: ["Kaleiçi", "Düden Şelalesi", "Konyaaltı Plajı"]
+    },
+    {
+        name: "Adana",
+        population: 2211250,
+        description: "Adana, Türkiye'nin Akdeniz Bölgesi'nde yer alan ve ünlü kebaplarıyla tanınan bir şehirdir.",
+        landmarks: ["Sabancı Merkez Camii", "Adana Arkeoloji Müzesi", "Seyhan Nehri"]
+    },
+    {
+        name: "Konya",
+        population: 2291128,
+        description: "Konya, Mevlana Celaleddin Rumi'nin memleketi olan ve tarihi zenginliklere sahip bir şehirdir.",
+        landmarks: ["Mevlana Müzesi", "Alaeddin Camii", "Sille Köyü"]
+    },
+    {
+        name: "Gaziantep",
+        population: 2049000,
+        description: "Gaziantep, mutfağıyla ünlü ve baklava gibi lezzetleriyle tanınan bir şehirdir.",
+        landmarks: ["Gaziantep Kalesi", "Zeugma Mozaik Müzesi", "Bakırcılar Çarşısı"]
+    },
+    {
+        name: "Kayseri",
+        population: 1085000,
+        description: "Kayseri, tarihi geçmişi ve sanayisi ile bilinen bir İç Anadolu şehridir.",
+        landmarks: ["Erciyes Dağı", "Kayseri Kalesi", "Hunat Hatun Camii"]
+    },
+    {
+        name: "Mersin",
+        population: 1800000,
+        description: "Mersin, Türkiye'nin güneyinde yer alan ve liman kenti olan bir şehirdir.",
+        landmarks: ["Tarsus Şelalesi", "Mersin Marina", "Kız Kalesi"]
+    },
+    {
+        name: "Sakarya",
+        population: 1037000,
+        description: "Sakarya, doğal güzellikleri ve sanayi ile bilinen bir şehirdir.",
+        landmarks: ["Sapanca Gölü", "Kartepe", "Sakarya Nehri"]
+    },
+    {
+        name: "Trabzon",
+        population: 786000,
+        description: "Trabzon, tarihi ve doğal güzellikleri ile tanınan bir Karadeniz şehridir.",
+        landmarks: ["Sümela Manastırı", "Uzungöl", "Atatürk Köşkü"]
+    },
+    {
+        name: "Edirne",
+        population: 166200,
+        description: "Edirne, Osmanlı İmparatorluğu döneminde önemli bir başkent olan tarihi bir şehirdir.",
+        landmarks: ["Selimiye Camii", "Edirne Kalesi", "Saray içi"]
+    },
+    {
+        name: "Manisa",
+        population: 1380000,
+        description: "Manisa, tarihi zenginlikleri ve bağcılığı ile ünlü bir şehirdir.",
+        landmarks: ["Manisa Kalesi", "Spil Dağı", "Kula Peri Bacaları"]
+    },
+    {
+        name: "Denizli",
+        population: 1000000,
+        description: "Denizli, Pamukkale Travertenleri ile ünlü olan bir şehirdir.",
+        landmarks: ["Pamukkale", "Hierapolis Antik Kenti", "Karahayıt"]
+    },
+    {
+        name: "Kocaeli",
+        population: 2000000,
+        description: "Kocaeli, sanayi ve ticaret merkezi olarak bilinen bir şehirdir.",
+        landmarks: ["Maşukiye", "Kartepe", "Sapanca Gölü"]
+    },
+    {
+        name: "Aydın",
+        population: 1110000,
+        description: "Aydın, tarihi kalıntıları ve doğal güzellikleri ile tanınan bir şehirdir.",
+        landmarks: ["Didyma Antik Kenti", "Aydın Arkeoloji Müzesi", "Kuşadası"]
+    },
+    {
+        name: "Bodrum",
+        population: 50000,
+        description: "Bodrum, Ege Bölgesi'nde yer alan ve turistik bir tatil beldesidir.",
+        landmarks: ["Bodrum Kalesi", "Antik Tiyatro", "Bodrum Marina"]
+    }
+];
+
+module.exports = sehirler;
