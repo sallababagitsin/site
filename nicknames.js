364cfaf5bb5df7b5b8d9d438e90cbf239f62a8dc
sallababagitsin <neyapimcom@gmail.com>
2024-12-07 11:36:35 +0300

    Update nicknames.js

--- nicknames.js
+++ nicknames.js
@@ -1,12 +1,72 @@
-module.exports = [
-    "ShadowHunter",
-    "PhoenixRider",
-    "DragonSlayer",
-    "MysticMage",
-    "SilentAssassin",
-    "FrostWizard",
-    "ThunderBeast",
-    "IronTitan",
-    "GoldenEagle",
-    "NightWolf"
-];
+module.exports = {
+    english: [
+        "ShadowHunter", "PhoenixRider", "DragonSlayer", "MysticMage", "SilentAssassin", 
+        "FrostWizard", "ThunderBeast", "IronTitan", "GoldenEagle", "NightWolf", 
+        "StormBringer", "DawnChaser", "NightShade", "SoulEater", "FireBender", 
+        "CrystalMage", "SilverWolf", "StormFury", "DarkKnight", "LunarPhoenix",
+        "WindWhisperer", "StarVoyager", "ShadowReaper", "IronClad", "SoulTaker",
+        "DarkWarden", "SilverPhoenix", "DemonSlayer", "StormBringer", "LunarWitch",
+        "NightShade", "SkyHunter", "FireSage", "VengefulMage", "EternalKnight",
+        "ShatteredSoul", "Frostbite", "ThunderStrike", "VenomousShadow", "MysticAssassin",
+        "BlazingDragon", "NightmareHunter", "DarkSorcerer", "BlightMage", "LoneWolf",
+        "SilverStorm", "IcePhoenix", "DoomBringer", "ShadowWarrior", "CelestialWitch"
+    ],
+    turkish: [
+        "AteşKanat", "GeceYelkeni", "KaraRüzgar", "FırtınaYolu", "BeyazGece",
+        "GölgeYolcusu", "IşıkKuşu", "KuzeyYıldızı", "YüksekRüzgar", "KahramanYol",
+        "YalnızSavaşçı", "DenizFırtınası", "BeyazKurt", "Karakış", "ŞimşekKöpeği",
+        "KaraGölge", "SonsuzRüzgar", "KızılAlev", "BeyazŞahin", "GeceAvcısı",
+        "ZümrütKuşu", "KaraFırtına", "KuzeyRüzgarı", "AydınlıkYol", "SavaşÇocuğu",
+        "GökBeyaz", "AlevSavaşçı", "BuzYıldızı", "FırtınaKanat", "GölgeBey",
+        "İzciYol", "DenizKurtları", "YükselenAlev", "GeceKara", "AlacakaranlıkKuşu",
+        "GökTuzak", "FırtınaAslanı", "BeyazFırtına", "YıldızKöpeği", "KurtKanat",
+        "ZeytinDal", "KüçükKartal", "KaraSavaşçı", "İzciKartalı", "BeyazŞimşek",
+        "GizliGece", "GölgeKartalı", "SonsuzKurt", "EfsaneRüzgar", "YıldızSavaşçı",
+        "KuşKalkan", "YüksekKartal", "KurtŞahin", "KıvrıkYelken", "FırtınaZamanı",
+        "KaranlıkYol", "EfsaneGölge", "SavaşcıRüzgar", "KuzeyKartalı", "KızılRüzgar",
+        "BeyazSavaşçı", "KurtGece", "AlevRüzgarı", "BeyazYelken", "KaraEfsane",
+        "GökRüzgarı", "BeyazBey", "YıldızKurt", "KüçükKurt", "AğırGök"
+    ],
+    spanish: [
+        "CazadorSombrío", "LadrónFénix", "DestructorDragón", "MagoMístico", "AsesinoSilencioso", 
+        "MagoGélido", "BestiaTormentosa", "TitánHierro", "ÁguilaOro", "LoboNoche", 
+        "PortadorTormenta", "CazadorAlba", "SombraNoche", "DevoraAlmas", "FuegoControlador", 
+        "MagoCristal", "LoboPlata", "TormentaFuria", "CaballeroOscuro", "FénixLunar",
+        "PortadorOscuridad", "DestructorCielo", "VientoFurioso", "LunaNoche", "MagoDragón",
+        "CazadorViento", "BendiciónFuego", "AlmaErrante", "AventureroSol", "SombraVenganza",
+        "CieloTormentoso", "LeónOscuro", "DragónFuego", "CaballeroLuz", "AlmaOscura",
+        "BestiaLuna", "TormentaViento", "CazadorEterna", "FuegoEterno", "VientoLuna",
+        "VengadorAlmas", "MagoLuz", "LuchadorOscuro", "VidenteTormenta", "NocheSombra"
+    ],
+    french: [
+        "ChasseurOmbre", "CavalierPhénix", "TueurDragon", "MageMystique", "AssassinSilencieux", 
+        "SorcierGlacé", "BêteFoudre", "TitanAcier", "AigleOr", "LoupNuit", 
+        "PorteurTempête", "ChasseurAube", "OmbreNuit", "DévoreurÂmes", "MaîtreFeu", 
+        "MageCristal", "LoupArgent", "TempêteFureur", "ChevalierOscuro", "PhénixLunaire",
+        "SageDesCieux", "TueurDeLumière", "ChasseurFoudre", "EspritLibéré", "OmbreDeLune",
+        "LameSanglante", "LoupDeGlace", "DragonDeFeu", "RoiDesNuits", "ChasseurDeLune",
+        "VoyageurDesOmbres", "OmbreRouge", "SorcierDeGlace", "VoleurD'âme", "ChevalierNoir",
+        "BêteDeLune", "PorteurDesRêves", "GardeDeL'Ombre", "SoleilNoir", "SombreDragon",
+        "FeuMaudit", "MagicienCéleste", "ChevalierDesTempêtes", "VengeurNuit", "OmbreSoleil"
+    ],
+    italian: [
+        "CacciatoreOmbra", "CavaliereFenice", "UccisoreDrago", "MagoMistico", "AssassinoSilenzioso", 
+        "MagoGelo", "BestiaTempesta", "TitanoFerro", "AquilaOro", "LupoNotte", 
+        "PortatoreTempesta", "CacciatoreAlba", "OmbraNotte", "DivoratoreAnime", "PadroneFiamme", 
+        "MagoCristallo", "LupoArgento", "TempestaFuria", "CavaliereOscuro", "FeniceLunare",
+        "VentoFurioso", "CavaliereDelLupo", "CacciatoreOscuro", "StellaFiamme", "TempestaDiLuna",
+        "MagoDelVento", "SoleNascente", "GuardianoOmbra", "SaggioOscuro", "DragonDiVento",
+        "AlbaInfuocata", "LupoDiSangue", "MagoDellaTempesta", "CacciatoreDelSole", "SerpenteMagico",
+        "FioreDellaTempesta", "VentoMagico", "LupoFiamme", "CorvoSanguinario", "CacciatoreDiFiamme"
+    ],
+    portuguese: [
+        "CaçadorSombra", "CavaleiroFênix", "MatadorDragão", "MagoMístico", "AssassinoSilencioso", 
+        "MagoGelo", "FeraTempestade", "TitãFerro", "ÁguiaDourada", "LoboNoite", 
+        "PortadorTempestade", "CaçadorAlvorada", "SombraNoite", "DevoradorAlmas", "MestreFogo", 
+        "MagoCristal", "LoboPrata", "TempestadeFúria", "CavaleiroSombrio", "FênixLunar",
+        "GuardiãoNoite", "LuzDaTempestade", "LoboFurioso", "MestreDragão", "VingadorAlmas",
+        "BatedorVento", "CaçadorFogo", "OlhadorSombrio", "LuzEterna", "CavaleiroVento",
+        "CaçadorNevoa", "TempestadeFogo", "VidenteAlmas", "FeiticeiroVento", "BestaMágica",
+        "MagiaDestrutiva", "GuerreiroFúria", "MestreLuz", "TempestadeGelo", "VingadorAlma"
+    ]
+};
